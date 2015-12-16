2e7f385a98d302932da48baf019c5084109cef5d
inickyap <yapnv@mcmaster.ca>
2015-12-15 23:17:49 -0500

    Update differential_expression.R

--- differential_expression.R
+++ differential_expression.R
@@ -47,83 +47,42 @@ plotPCA(pca_analysis, intgroup=c("treatment"))
 #Comparing each group with the control treatment 4 
 
 #LPS_LPS vs vec_vec mice
-DESeq_data_1_4 <- DESeqDataSetFromMatrix(tot_count_matrix, experimental_design, design = formula(~treatment + age))
+DESeq_data_1_4 <- DESeqDataSetFromMatrix(tot_count_matrix, experimental_design, design = formula(~treatment + age + treatment:age))
+DESeq_data_1_4$treatment <- relevel(DESeq_data_1_4$treatment, ref= "4")
 DESeq_data_1_4 <- DESeq(DESeq_data_1_4)
-treatment_results_1_vs_4 <- results(DESeq_data_1_4, contrast=c("treatment", "1", "4"), pAdjustMethod="BH" , alpha= 0.05)
+resultsNames(DESeq_data_1_4)
+treatment_results_1_vs_4 <- results(DESeq_data_1_4, contrast=list("treatment_1_vs_4", "age_Y_vs_O"), pAdjustMethod="BH" , alpha= 0.05)
 summary(treatment_results_1_vs_4)
 treatment_results_1_vs_4_sorted <- treatment_results_1_vs_4[order(treatment_results_1_vs_4$padj),]
 
 write.csv(treatment_results_1_vs_4_sorted, file="treatment_diff_1_vs_4")
-write.csv(treatment_results_1_vs_4, file="treatment_diff_name_1_vs_4")
 plotDispEsts(DESeq_data_1_4, xlab="Mean of Normalized Counts", ylab="Dispersion", main="Mean Dispersion")
-plotMA(DESeq_data_1_4, ylim=c(-5,5), main="LPS LPS vs vec vec")
+plotMA(treatment_results_1_vs_4, ylim=c(-10,10), main="LPS LPS vs vec vec")
 
 #LPS_vec vs vec_vec mice
-DESeq_data_2_4 <- DESeqDataSetFromMatrix(tot_count_matrix, experimental_design, design = formula(~treatment + age))
+DESeq_data_2_4 <- DESeqDataSetFromMatrix(tot_count_matrix, experimental_design, design = formula(~treatment + age + treatment:age))
+DESeq_data_2_4$treatment <- relevel(DESeq_data_2_4$treatment, ref= "4")
 DESeq_data_2_4 <- DESeq(DESeq_data_2_4)
-treatment_results_2_vs_4 <- results(DESeq_data_2_4, contrast=c("treatment", "2", "4"), pAdjustMethod="BH" , alpha= 0.05)
+resultsNames(DESeq_data_2_4)
+treatment_results_2_vs_4 <- results(DESeq_data_2_4, contrast=list("treatment_2_vs_4", "age_Y_vs_O"), pAdjustMethod="BH" , alpha= 0.05)
 summary(treatment_results_2_vs_4)
-treatment_results_2_vs_4_sorted <- treatment_results_3_vs_4[order(treatment_results_2_vs_4$padj),]
+treatment_results_2_vs_4_sorted <- treatment_results_2_vs_4[order(treatment_results_2_vs_4$padj),]
 
 write.csv(treatment_results_2_vs_4_sorted, file="treatment_diff_2_vs_4")
-write.csv(treatment_results_2_vs_4, file="treatment_diff_name_2_vs_4")
 plotDispEsts(DESeq_data_2_4, xlab="Mean of Normalized Counts", ylab="Dispersion", main="Mean Dispersion")
-plotMA(DESeq_data_2_4, ylim=c(-5,5), main="LPS LPS vs vec vec")
+plotMA(treatment_results_2_vs_4, ylim=c(-10,10), main="LPS vec vs vec vec")
 
 
 #vec_LPS vs vec_vec mice
-DESeq_data_3_4 <- DESeqDataSetFromMatrix(tot_count_matrix, experimental_design, design = formula(~treatment + age))
+DESeq_data_3_4 <- DESeqDataSetFromMatrix(tot_count_matrix, experimental_design, design = formula(~treatment + age + treatment:age))
+DESeq_data_3_4$treatment <- relevel(DESeq_data_3_4$treatment, ref= "4")
 DESeq_data_3_4 <- DESeq(DESeq_data_3_4)
-treatment_results_3_vs_4 <- results(DESeq_data_3_4, contrast=c("treatment", "3", "4"), pAdjustMethod="BH" , alpha= 0.05)
+resultsNames(DESeq_data_3_4)
+treatment_results_3_vs_4 <- results(DESeq_data_3_4, contrast=list("treatment_3_vs_4", "age_Y_vs_O"), pAdjustMethod="BH" , alpha= 0.05)
 summary(treatment_results_3_vs_4)
 treatment_results_3_vs_4_sorted <- treatment_results_3_vs_4[order(treatment_results_3_vs_4$padj),]
 
 write.csv(treatment_results_3_vs_4_sorted, file="treatment_diff_3_vs_4")
-write.csv(treatment_results_3_vs_4, file="treatment_diff_name_3_vs_4")
 plotDispEsts(DESeq_data_3_4, xlab="Mean of Normalized Counts", ylab="Dispersion", main="Mean Dispersion")
-plotMA(DESeq_data_3_4, ylim=c(-5,5), main="vec LPS vs vec vec")
-
-
-
-
-
-#Comparisons between groups 
-
-# LPS_LPS vs LPS_vec
-DESeq_data_1_2 <- DESeqDataSetFromMatrix(tot_count_matrix, experimental_design, design = formula(~treatment + age))
-DESeq_data_1_2 <- DESeq(DESeq_data_1_2)
-treatment_results_1_vs_2 <- results(DESeq_data_1_2, contrast=c("treatment", "1", "2"), pAdjustMethod="BH" , alpha= 0.05)
-summary(treatment_results_1_vs_2)
-treatment_results_1_vs_2_sorted <- treatment_results_1_vs_2[order(treatment_results_1_vs_2$padj),]
-
-write.csv(treatment_results_1_vs_2_sorted, file="treatment_diff_1_vs_2")
-plotDispEsts(DESeq_data_1_2, xlab="Mean of Normalized Counts", ylab="Dispersion", main="Mean Dispersion")
-plotMA(DESeq_data_1_2, ylim=c(-5,5), main="Differential Expression between LPS_LPS and LPS_Vec")
-
-
-#LPS_LPS vs vec_LPS mice
-DESeq_data_1_3 <- DESeqDataSetFromMatrix(tot_count_matrix, experimental_design, design = formula(~treatment + age))
-DESeq_data_1_3 <- DESeq(DESeq_data_1_3)
-treatment_results_1_vs_3 <- results(DESeq_data_1_3, contrast=c("treatment", "1", "3"), pAdjustMethod="BH" , alpha= 0.05)
-summary(treatment_results_1_vs_3)
-treatment_results_1_vs_3_sorted <- treatment_results_1_vs_3[order(treatment_results_1_vs_3$padj),]
-
-write.csv(treatment_results_1_vs_3_sorted, file="treatment_diff_1_vs_3")
-write.csv(treatment_results_1_vs_3, file="treatment_diff_name_1_vs_3")
-plotDispEsts(DESeq_data_1_3, xlab="Mean of Normalized Counts", ylab="Dispersion", main="Mean Dispersion")
-plotMA(DESeq_data_1_3, ylim=c(-5,5), main="Differential Expression Between LPS_LPS and Vec_LPS")
-
-
-#LPS_vec vs vec_LPS mice
-DESeq_data_2_3 <- DESeqDataSetFromMatrix(tot_count_matrix, experimental_design, design = formula(~treatment + age))
-DESeq_data_2_3 <- DESeq(DESeq_data_2_3)
-treatment_results_2_vs_3 <- results(DESeq_data_2_3, contrast=c("treatment", "2", "3"), pAdjustMethod="BH" , alpha= 0.05)
-summary(treatment_results_2_vs_3)
-treatment_results_2_vs_3_sorted <- treatment_results_2_vs_3[order(treatment_results_2_vs_3$padj),]
-
-write.csv(treatment_results_2_vs_3_sorted, file="treatment_diff_2_vs_3")
-write.csv(treatment_results_2_vs_3, file="treatment_diff_name_2_vs_3")
-plotDispEsts(DESeq_data_2_3, xlab="Mean of Normalized Counts", ylab="Dispersion", main="Mean Dispersion")
-plotMA(DESeq_data_2_3, ylim=c(-5,5), main="LPS vec vs vec LPS")
-
+plotMA(treatment_results_3_vs_4, ylim=c(-10,10), main="vec LPS vs vec vec")
 
